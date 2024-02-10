2ca8b6ff3aacb8eaa3ff52ed7f30c9e2a4a2e8cb
shamshadhusain3 <111483127+shamshadhusain3@users.noreply.github.com>
2024-02-10 20:51:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const routes=require('./routes/routes');
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-<<<<<<< HEAD
+
 app.use('https://shortlinkapp.onrender.com/',routes)
-=======
-app.use('https://shortlinkapp.onrender.com/',routes)
->>>>>>> 03769d2e51afe9fc7a9f4f6690f6c9abf642c75d
+
 
 app.set('view engine','ejs')
 
